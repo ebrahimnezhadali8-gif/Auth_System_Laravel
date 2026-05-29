e8bffb83885c2448dff1ebb2c686f8869512e6da
Ali <ebrahimnezhadali8@gmail.com>
2026-05-29 17:36:16 +0330

    create login endpoint :
    - update table personal_access_tokens
    - create endpoint login and create access token and refresh token
    - set cookies

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -28,9 +29,80 @@ class AuthController extends Controller
             'phone' => $request->phone,
             'password' => Hash::make($request->password),
         ]);
-        return successResponse(new UserResource($user), 201, 'User registered successfully');
+        return successResponse(new UserResource($user), 200, 'User registered successfully');
     }
-    public function login(Request $request) {}
+
+    public function login(Request $request)
+    {
+        // Validation
+        $validator = Validator::make($request->all(), [
+            "phone"    => 'required|string',
+            "password" => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return errorResponse(422, $validator->messages());
+        }
+
+        // check if user exists and password is correct
+        $user = User::where('phone', $request->phone)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return errorResponse(401, 'phone or password is incorrect');
+        }
+
+        // create deviceId 
+        $deviceId = Str::uuid();
+
+        // create Access Token 
+        $accessToken = $user->createToken(
+            'access_'  . $deviceId,
+            ['access'],
+            now()->addMinutes((int) env('ACCESS_TOKEN_EXPIRY', 15))
+        )->plainTextToken;
+
+        // create Refresh Token
+        $refreshToken = $user->createToken(
+            'refresh_' . $deviceId,
+            ['refresh'],
+            now()->addMinutes((int) env('REFRESH_TOKEN_EXPIRY', 10080))
+        )->plainTextToken;
+
+        // create HttpOnly Cookie 
+        $accessCookie = cookie(
+            'access_token',
+            $accessToken,
+            env('ACCESS_TOKEN_EXPIRY', 15),
+            '/',    // path 
+            null,   // domain 
+            true,   // secure
+            true    // httpOnly
+        );
+
+        $refreshCookie = cookie(
+            'refresh_token',
+            $refreshToken,
+            env('REFRESH_TOKEN_EXPIRY', 10080),
+            '/',
+            null,
+            true,
+            true
+        );
+
+        $deviceCookie = cookie(
+            'device_id',
+            $deviceId,
+            env('REFRESH_TOKEN_EXPIRY', 10080),
+            '/',
+            null,
+            true,
+            true
+        );
+
+        return successResponse(new UserResource($user), 201, 'User login successfully')
+            ->withCookie($accessCookie)
+            ->withCookie($refreshCookie)
+            ->withCookie($deviceCookie);
+    }
     public function refresh(Request $request) {}
     public function logout(Request $request) {}
 }
--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,9 @@ Route::prefix('auth')->group(function () {
 
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login',    [AuthController::class, 'login']);
-
+    Route::post('refresh', [AuthController::class, 'refresh']);
     // with middleware
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout',  [AuthController::class, 'logout']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
     });
 });
